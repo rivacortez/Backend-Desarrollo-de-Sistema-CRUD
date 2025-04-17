6429095c8bd77826ec0011751da589aed0d092e0
rivacortezf2006 <u202215313@upc.edu.pe>
2025-04-17 16:19:05 -0500

    feat(customers): add CustomersQueryServiceImpl

--- app/ReservationManagement/Customers/application/internal/queryservices/CustomersQueryServiceImpl.php
+++ app/ReservationManagement/Customers/application/internal/queryservices/CustomersQueryServiceImpl.php
@@ -1,1 +1,31 @@
 <?php
+
+namespace App\ReservationManagement\Customers\Application\Internal\QueryServices;
+
+use App\ReservationManagement\Customers\Domain\Services\CustomersQueryService;
+use App\ReservationManagement\Customers\Domain\Model\Queries\GetAllCustomersQuery;
+use App\ReservationManagement\Customers\Domain\Model\Queries\GetAllCustomersByIdQuery;
+use App\ReservationManagement\Customers\Infrastructure\Persistence\CustomersRepository;
+
+
+//TODO:implement CustomerRepository
+
+class CustomersQueryServiceImpl implements CustomersQueryService
+{
+    private $repository;
+
+    public function __construct(CustomersRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function execute(GetAllCustomersQuery $query)
+    {
+        return $this->repository->getAll();
+    }
+
+    public function executeById(GetAllCustomersByIdQuery $query)
+    {
+        return $this->repository->getById($query->id);
+    }
+}
