b5f84f802f12668b35b57f6814cd2fb5185d2d8e
rivacortezf2006 <u202215313@upc.edu.pe>
2025-04-17 18:06:44 -0500

    feat(tables): add queries & command services implementation

--- app/ReservationManagement/Tables/application/internal/commandservices/TablesCommandServiceImpl.php
+++ app/ReservationManagement/Tables/application/internal/commandservices/TablesCommandServiceImpl.php
@@ -1,1 +1,38 @@
 <?php
+
+namespace App\ReservationManagement\Tables\application\internal\commandservices;
+
+use App\ReservationManagement\Tables\Domain\Services\TablesCommandService;
+use App\ReservationManagement\Tables\Domain\Model\Commands\TablesCommand;
+use Illuminate\Support\Facades\Validator;
+
+class TablesCommandServiceImpl implements TablesCommandService
+{
+    private $repository;
+
+    //TODO: IMPLEMENT REPOSITORY
+    public function __construct(TablesRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function handle(TablesCommand $command)
+    {
+        $data = [
+            'numero_mesa' => $command->numero_mesa,
+            'capacidad' => $command->capacidad,
+            'ubicacion' => $command->ubicacion,
+        ];
+
+        if ($command->id) {
+            return $this->repository->update($command->id, $data);
+        } else {
+            return $this->repository->store($data);
+        }
+    }
+
+    public function delete($id)
+    {
+        return $this->repository->delete($id);
+    }
+}
--- app/ReservationManagement/Tables/application/internal/queryservices/TablesQueryServiceImpl.php
+++ app/ReservationManagement/Tables/application/internal/queryservices/TablesQueryServiceImpl.php
@@ -1,1 +1,27 @@
 <?php
+
+namespace App\ReservationManagement\Tables\application\internal\queryservices;
+
+use App\ReservationManagement\Tables\Domain\Model\Queries\GetAllTablesByIdQuery;
+use App\ReservationManagement\Tables\Domain\Model\Queries\GetAllTablesQuery;
+use App\ReservationManagement\Tables\Domain\Services\TablesQueryService;
+
+class TablesQueryServiceImpl implements TablesQueryService
+{
+    private $repository;
+//TODO: add type hint for repository
+    public function __construct(TablesRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function execute(GetAllTablesQuery $query)
+    {
+        return $this->repository->getAll();
+    }
+
+    public function executeById(GetAllTablesByIdQuery $query)
+    {
+        return $this->repository->getById($query->id);
+    }
+}
