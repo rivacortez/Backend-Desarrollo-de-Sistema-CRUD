35274a3e3498d437fdb73ea87d4cc8c59a28a918
rivacortezf2006 <u202215313@upc.edu.pe>
2025-04-17 18:10:12 -0500

    feat(tables): add Table resource

--- app/ReservationManagement/Tables/interfaces/rest/resources/TablesResource.php
+++ app/ReservationManagement/Tables/interfaces/rest/resources/TablesResource.php
@@ -1,1 +1,20 @@
 <?php
+
+namespace App\ReservationManagement\Tables\interfaces\rest\resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TablesResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'numero_mesa' => $this->numero_mesa,
+            'capacidad' => $this->capacidad,
+            'ubicacion' => $this->ubicacion,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
