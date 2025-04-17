b95bc2a741f23b0e32eb35d8915255759c73dea9
rivacortezf2006 <u202215313@upc.edu.pe>
2025-04-17 16:15:17 -0500

    feat(customers): add CustomersQueryService

--- app/ReservationManagement/Customers/domain/services/CustomersQueryService.php
+++ app/ReservationManagement/Customers/domain/services/CustomersQueryService.php
@@ -1,1 +1,12 @@
 <?php
+
+namespace App\ReservationManagement\Customers\Domain\Services;
+
+use App\ReservationManagement\Customers\Domain\Model\Queries\GetAllCustomersQuery;
+use App\ReservationManagement\Customers\Domain\Model\Queries\GetAllCustomersByIdQuery;
+
+interface CustomersQueryService
+{
+    public function execute(GetAllCustomersQuery $query);
+    public function executeById(GetAllCustomersByIdQuery $query);
+}
