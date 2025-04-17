6bfd6afcb043b0aea7bad2d514bfcb08bb3b8302
rivacortezf2006 <u202215313@upc.edu.pe>
2025-04-17 16:14:53 -0500

    feat(customers): add CustomersCommandService

--- app/ReservationManagement/Customers/domain/services/CustomersCommandService.php
+++ app/ReservationManagement/Customers/domain/services/CustomersCommandService.php
@@ -1,1 +1,11 @@
 <?php
+
+namespace App\ReservationManagement\Customers\Domain\Services;
+
+use App\ReservationManagement\Customers\Domain\Model\Commands\CustomerCommand;
+
+interface CustomersCommandService
+{
+    public function handle(CustomerCommand $command);
+    public function delete($id);
+}
