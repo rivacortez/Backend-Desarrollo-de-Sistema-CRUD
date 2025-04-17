de07c57353ba67c609d0b9db7e791a05bbf9b969
rivacortezf2006 <u202215313@upc.edu.pe>
2025-04-17 16:14:22 -0500

    feat(customers): add GetAllCustomersByIdQuery

--- app/ReservationManagement/Customers/domain/model/ queries/GetAllCustomersByIdQuery.php
+++ app/ReservationManagement/Customers/domain/model/ queries/GetAllCustomersByIdQuery.php
@@ -1,1 +1,13 @@
 <?php
+
+namespace App\ReservationManagement\Customers\Domain\Model\Queries;
+
+class GetAllCustomersByIdQuery
+{
+    public $id;
+
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
+}
