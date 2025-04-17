e5df331d3e12aa0d3c3e78a47049fc5240c268db
rivacortezf2006 <u202215313@upc.edu.pe>
2025-04-17 16:21:20 -0500

    feat(customers): add CustomersResource

--- app/ReservationManagement/Customers/interfaces/rest/resources/CustomersResource.php
+++ app/ReservationManagement/Customers/interfaces/rest/resources/CustomersResource.php
@@ -1,1 +1,21 @@
 <?php
+
+namespace App\ReservationManagement\Customers\Interfaces\Rest\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CustomersResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'nombre' => $this->nombre,
+            'correo' => $this->correo,
+            'telefono' => $this->telefono,
+            'direccion' => $this->direccion,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
