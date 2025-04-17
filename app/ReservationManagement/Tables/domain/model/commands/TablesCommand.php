7a3fe61c730d2273b519220cdd349b69c3096d0b
rivacortezf2006 <u202215313@upc.edu.pe>
2025-04-17 17:59:14 -0500

    feat(tables): add TablesCommand

--- app/ReservationManagement/Tables/domain/model/commands/TablesCommand.php
+++ app/ReservationManagement/Tables/domain/model/commands/TablesCommand.php
@@ -1,1 +1,19 @@
 <?php
+
+namespace App\ReservationManagement\Tables\domain\model\commands;
+
+class TablesCommand
+{
+    public $id;
+    public $numero_mesa;
+    public $capacidad;
+    public $ubicacion;
+
+    public function __construct($id = null, $numero_mesa = null, $capacidad = null, $ubicacion = null)
+    {
+        $this->id = $id;
+        $this->numero_mesa = $numero_mesa;
+        $this->capacidad = $capacidad;
+        $this->ubicacion = $ubicacion;
+    }
+}
