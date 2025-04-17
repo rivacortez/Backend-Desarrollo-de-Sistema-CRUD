439cd56beb3842daa6b9a4605fb909c8cf4e5ac5
rivacortezf2006 <u202215313@upc.edu.pe>
2025-04-17 18:03:57 -0500

    feat(tables): add command & queries services

--- app/ReservationManagement/Tables/domain/services/TablesCommandService.php
+++ app/ReservationManagement/Tables/domain/services/TablesCommandService.php
@@ -1,1 +1,11 @@
 <?php
+
+namespace App\ReservationManagement\Tables\domain\services;
+
+use App\ReservationManagement\Tables\Domain\Model\Commands\TablesCommand;
+
+interface TablesCommandService
+{
+    public function handle(TablesCommand $command);
+    public function delete($id);
+}
--- app/ReservationManagement/Tables/domain/services/TablesQueryService.php
+++ app/ReservationManagement/Tables/domain/services/TablesQueryService.php
@@ -1,1 +1,12 @@
 <?php
+
+namespace App\ReservationManagement\Tables\domain\services;
+
+use App\ReservationManagement\Tables\Domain\Model\Queries\GetAllTablesQuery;
+use App\ReservationManagement\Tables\Domain\Model\Queries\GetAllTablesByIdQuery;
+
+interface TablesQueryService
+{
+    public function execute(GetAllTablesQuery $query);
+    public function executeById(GetAllTablesByIdQuery $query);
+}
