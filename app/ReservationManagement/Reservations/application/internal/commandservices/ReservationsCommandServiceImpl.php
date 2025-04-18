7222849da94ef364441aad238bdf6b32556044da
rivacortezf2006 <u202215313@upc.edu.pe>
2025-04-17 19:10:28 -0500

    feat(reservations): add command & query implementation

--- app/ReservationManagement/Reservations/application/internal/commandservices/ReservationsCommandServiceImpl.php
+++ app/ReservationManagement/Reservations/application/internal/commandservices/ReservationsCommandServiceImpl.php
@@ -1,1 +1,39 @@
 <?php
+
+namespace App\ReservationManagement\Reservations\application\internal\commandservices;
+
+use App\ReservationManagement\Reservations\domain\services\ReservationsCommandService;
+use App\ReservationManagement\Reservations\domain\model\commands\ReservationsCommand;
+use App\ReservationManagement\Reservations\infrastructure\persistence\ReservationsRepository;
+
+class ReservationsCommandServiceImpl implements ReservationsCommandService
+{
+    private $repository;
+
+    public function __construct(ReservationsRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function handle(ReservationsCommand $command)
+    {
+        $data = [
+            'fecha' => $command->fecha,
+            'hora' => $command->hora,
+            'numero_de_personas' => $command->numero_de_personas,
+            'comensal_id' => $command->comensal_id,
+            'mesa_id' => $command->mesa_id,
+        ];
+
+        if ($command->id) {
+            return $this->repository->update($command->id, $data);
+        } else {
+            return $this->repository->store($data);
+        }
+    }
+
+    public function delete($id)
+    {
+        return $this->repository->delete($id);
+    }
+}
--- app/ReservationManagement/Reservations/application/internal/queryservices/ReservationsQueryServiceImpl.php
+++ app/ReservationManagement/Reservations/application/internal/queryservices/ReservationsQueryServiceImpl.php
@@ -1,1 +1,28 @@
 <?php
+
+namespace App\ReservationManagement\Reservations\application\internal\queryservices;
+
+use App\ReservationManagement\Reservations\domain\model\queries\GetAllReservationsByIdQuery;
+use App\ReservationManagement\Reservations\domain\model\queries\GetAllReservationsQuery;
+use App\ReservationManagement\Reservations\domain\services\ReservationsQueryService;
+use App\ReservationManagement\Reservations\infrastructure\persistence\ReservationsRepository;
+
+class ReservationsQueryServiceImpl implements ReservationsQueryService
+{
+    private $repository;
+
+    public function __construct(ReservationsRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function execute(GetAllReservationsQuery $query)
+    {
+        return $this->repository->getAll();
+    }
+
+    public function executeById(GetAllReservationsByIdQuery $query)
+    {
+        return $this->repository->getById($query->id);
+    }
+}
