362407db6d086de915cf7b47dd9325a164a1d21e
rivacortezf2006 <u202215313@upc.edu.pe>
2025-04-17 19:11:09 -0500

    feat(reservations): add reservations resource

--- app/ReservationManagement/Reservations/interfaces/rest/resources/ReservationsResource.php
+++ app/ReservationManagement/Reservations/interfaces/rest/resources/ReservationsResource.php
@@ -1,1 +1,24 @@
 <?php
+
+namespace App\ReservationManagement\Reservations\interfaces\rest\resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReservationsResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'fecha' => $this->fecha,
+            'hora' => $this->hora,
+            'numero_de_personas' => $this->numero_de_personas,
+            'comensal_id' => $this->comensal_id,
+            'comensal' => $this->whenLoaded('comensal'),
+            'mesa_id' => $this->mesa_id,
+            'mesa' => $this->whenLoaded('mesa'),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
