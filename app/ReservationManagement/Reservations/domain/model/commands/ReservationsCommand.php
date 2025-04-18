63c16d80000bbb70a88fb7bc05a5aead1c59db1e
rivacortezf2006 <u202215313@upc.edu.pe>
2025-04-17 19:06:10 -0500

    feat(reservations): add reservationCommand

--- app/ReservationManagement/Reservations/domain/model/commands/ReservationsCommand.php
+++ app/ReservationManagement/Reservations/domain/model/commands/ReservationsCommand.php
@@ -1,1 +1,23 @@
 <?php
+
+namespace App\ReservationManagement\Reservations\domain\model\commands;
+
+class ReservationsCommand
+{
+    public $id;
+    public $fecha;
+    public $hora;
+    public $numero_de_personas;
+    public $comensal_id;
+    public $mesa_id;
+
+    public function __construct($id = null, $fecha = null, $hora = null, $numero_de_personas = null, $comensal_id = null, $mesa_id = null)
+    {
+        $this->id = $id;
+        $this->fecha = $fecha;
+        $this->hora = $hora;
+        $this->numero_de_personas = $numero_de_personas;
+        $this->comensal_id = $comensal_id;
+        $this->mesa_id = $mesa_id;
+    }
+}
