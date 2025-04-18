01e7408cc9c6d8287b7b9167a64aafe7c1ccbd30
rivacortezf2006 <u202215313@upc.edu.pe>
2025-04-17 19:07:02 -0500

    feat(reservations): add domain queries

--- app/ReservationManagement/Reservations/domain/model/ queries/GetAllReservationsQuery.php
+++ app/ReservationManagement/Reservations/domain/model/ queries/GetAllReservationsQuery.php
@@ -1,1 +1,10 @@
 <?php
+
+namespace App\ReservationManagement\Reservations\domain\model\queries;
+
+class GetAllReservationsQuery
+{
+    public function __construct()
+    {
+    }
+}
--- app/ReservationManagement/Reservations/domain/model/ queries/GetAllReservationssByIdQuery.php
+++ app/ReservationManagement/Reservations/domain/model/ queries/GetAllReservationssByIdQuery.php
@@ -1,1 +1,13 @@
 <?php
+
+namespace App\ReservationManagement\Reservations\domain\model\queries;
+
+class GetAllReservationsByIdQuery
+{
+    public $id;
+
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
+}
