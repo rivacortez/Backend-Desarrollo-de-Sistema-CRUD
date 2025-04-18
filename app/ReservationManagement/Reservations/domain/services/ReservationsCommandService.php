2d5cb67e5864750b44efb9a776544ed11b6b7de3
rivacortezf2006 <u202215313@upc.edu.pe>
2025-04-17 19:07:50 -0500

    feat(reservations): add command & queries services

--- app/ReservationManagement/Reservations/domain/services/ReservationsCommandService.php
+++ app/ReservationManagement/Reservations/domain/services/ReservationsCommandService.php
@@ -1,1 +1,11 @@
 <?php
+
+namespace App\ReservationManagement\Reservations\domain\services;
+
+use App\ReservationManagement\Reservations\domain\model\commands\ReservationsCommand;
+
+interface ReservationsCommandService
+{
+    public function handle(ReservationsCommand $command);
+    public function delete($id);
+}
--- app/ReservationManagement/Reservations/domain/services/ReservationsQueryService.php
+++ app/ReservationManagement/Reservations/domain/services/ReservationsQueryService.php
@@ -1,1 +1,12 @@
 <?php
+
+namespace App\ReservationManagement\Reservations\domain\services;
+
+use App\ReservationManagement\Reservations\domain\model\queries\GetAllReservationsQuery;
+use App\ReservationManagement\Reservations\domain\model\queries\GetAllReservationsByIdQuery;
+
+interface ReservationsQueryService
+{
+    public function execute(GetAllReservationsQuery $query);
+    public function executeById(GetAllReservationsByIdQuery $query);
+}
